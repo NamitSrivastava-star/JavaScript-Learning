7902917c2a07ba346203edaa592b5fa3d0aaf3f2
NamitSrivastava-star <65862046+NamitSrivastava-star@users.noreply.github.com>
2024-06-16 10:38:20 +0000

    add string

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,12 @@
+let name = "Rahul"
+// console.log(name);
+name = 'Shyam'
+// console.log(name);
+
+const userName = new String('Mohan')
+console.log(userName);
+// console.log(typeof userName);
+console.log(userName.length);
+console.log(userName.slice(-3, 4));
+
+console.log(`My user name is ${userName}`); // String Interpolation
